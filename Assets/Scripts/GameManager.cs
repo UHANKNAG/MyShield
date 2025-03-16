f52e0049b3f5b5ca2b385994d43fcebaafe815c3
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-03-16 22:33:21 +0900

    Set Time Txt

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject square;
+    public Text timeTxt;
+    float time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
     }
 
     void MakeSquare() {
