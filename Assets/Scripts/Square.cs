5439f68c9122161cda984e26699b6ed3bb7d1489
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-03-16 22:06:22 +0900

    Add Random Variable

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-3.0f, 3.0f);
+        float y = Random.Range(3.0f, 5.0f);
+        float size = Random.Range(0.5f, 1.5f);
+
+        transform.position = new Vector2(x, y);
+        transform.localScale = new Vector2(size, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
