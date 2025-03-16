c71a8d05c482dd1ab93943f518de44652f5c60df
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-03-16 22:29:56 +0900

    Create Square Prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeSquare", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeSquare() {
+        Instantiate(square);
+    }
+}
