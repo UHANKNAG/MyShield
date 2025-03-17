ad3fef082088f202fa9ad9173a6f570684081e81
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-03-17 22:55:56 +0900

    Set Retry Button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,24 +5,44 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
     public GameObject square;
+    public GameObject endPanel;
     public Text timeTxt;
+    public Text nowScore;
     float time = 0.0f;
+
+    bool isPlay = true;
+
+    private void Awake() {
+        if (Instance == null)
+            Instance = this;    
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1.0f;
         InvokeRepeating("MakeSquare", 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeTxt.text = time.ToString("N2");
+        if (isPlay) {
+            time += Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+        }
     }
 
     void MakeSquare() {
         Instantiate(square);
+    }
+
+    public void GameOver() {
+        isPlay = false;
+        Time.timeScale = 0.0f;
+        nowScore.text = time.ToString("N2");
+        endPanel.SetActive(true);
     }
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -20,4 +20,10 @@ public class Square : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            GameManager.Instance.GameOver();
+        }
+    }
 }
